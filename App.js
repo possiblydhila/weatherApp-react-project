b17f7343a8bd4a93b0f5e436adc44af54fa0a4e2
possiblydhila <fadhilapurboningrum@gmail.com>
2024-07-07 12:00:23 +0700

    commit

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+// Import axios, BASE_URL, dan API_KEY
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
+import { View, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
 
-export default function App() {
+const App = () => {
+
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState(null)
+
+  // Definisikan state status
+  const [status, setStatus] = useState('')
+
+  const searchWeather = (location) => {
+    // Mengatur status ke "loading"
+    setStatus('loading')
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        // Tambahkan code di bawah
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+        // Mengatur status ke "success"
+        setStatus('success')
+      })
+      .catch((error) => {
+        // Mengatur status ke "error"
+        setStatus('error')
+      })
+  }
+
+  // Definisikan function renderComponent
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff', marginTop: 64 }}>
+      <View style={styles.container}>
+        <WeatherSearch searchWeather={searchWeather} />
+        {/* Tampilkan data cuaca ketika ada weatherData */}
+        {/* {weatherData && <WeatherInfo weatherData={weatherData} />} */}
+
+        {/* Menggunakan function renderComponent di sini */}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
-});
+})
+
+export default App
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = '6f3612aa8dbfaf73c2f4b4b04e055044';
